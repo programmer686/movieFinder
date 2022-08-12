b17eeadc57ec779b8f0547b45ebd41f75838f3d4
programmer686 <105795650+programmer686@users.noreply.github.com>
2022-08-12 17:26:08 -0500

    my changes

--- index.js
+++ index.js
@@ -1,50 +1,25 @@
-const { waitUntilSymbol } = ("next/dist/server/web/spec-compliant/fetch-event");
- // window.localStorage.clear()
+const { waitUntilSymbol } = "next/dist/server/web/spec-compliant/fetch-event";
+// window.localStorage.clear()
 let search = document
   .getElementById("search")
   .addEventListener("change", (event) => {
     searchVal = event.target.value;
-});
-let searchBtn = document.getElementById("searchBtn")
+  });
+let searchBtn = document.getElementById("searchBtn");
 let result = document.getElementById("results");
-let watchlist = document.getElementById("watchList");
-const switched = document.getElementById("switch");
 let card = document.getElementById("card");
 
-
-
 function handleEnter(ele) {
-  if(event.key === "Enter"){
-      handleChange()
+  if (event.key === "Enter") {
+    handleChange();
   }
 }
-
 
 let searchVal = "";
-let watchListSaved = "";
-let display = true;
-watchlist.style.display = "none";
-function handleSwtich() {
-  display ? (display = false) : (display = true);
-  if (display) {
-    watchlist.style.display = "none";
-    card.style.display = "flex";
-    result.style.display = "flex";
-    switched.textContent = "My Watchlist";
-  } else {
-    card.style.display = "none";
-    result.style.display = "none";
-    watchlist.style.display = "flex";
-    switched.textContent = "Search";
-  }
-}
-
-// what i had origianlly
-let storingInfo =  []
+let storingInfo = JSON.parse(window.localStorage.getItem("movieData")) || [];
 function storingData(id, name) {
-  storingInfo.unshift([name, id])
-  window.localStorage.setItem("movieData", storingInfo)
-  handleLoading();
+  storingInfo.unshift([name, id]);
+  window.localStorage.setItem("movieData", JSON.stringify(storingInfo));
 }
 function handleChange() {
   result.innerHTML = "";
@@ -90,35 +65,4 @@ function handleChange() {
     });
 }
 
-function handleLoading(){
-  watchlist.innerHTML = ""
-  for (let i in storingInfo) {
-    fetch(`https://www.omdbapi.com/?i=${storingInfo[i][1]}&plot=short&apikey=95ff048`)
-    .then((jsoned) => jsoned.json())
-    .then((data) => {
-      console.log(data)
-      watchlist.innerHTML += `
-                    <div class="movie--card save--movie" >
-                        <div class="movie--body save--movie--body">
-                            <div class="controlImg">
-                                <img class="movie--poster" src=${data.Poster} />
-                            </div>
-                            <div class="info--container saved--info--container">
-                                <h1 class="movie--title">${data.Title}</h1>
-                                <div class="genreRuntime--container">
-                                    <p class="movie--genre">${data.Genre}</p>
-                                    <p class="info--separators">|</p>
-                                    <p class="movie--duration">${data.Runtime}</p>
-                                    <p class="info--separators">|</p>
-                                    <p class="movie--rating">${data.imdbRating}</p><p class="star">⭐</p>
-                                </div>
-                                <p class="movie--plot">${data.Plot}</p>
-                                 <span></span>
-                            </div>
-                        </div>
-                   </div>
-                   <span></span> `;  })
-  }}
 
-
-  handleLoading()
---
+++ watchList.js
@@ -0,0 +1,60 @@
+let watchlist = document.getElementById("watchList");
+let data = JSON.parse(window.localStorage.getItem("movieData"));
+console.log(data.length == 0);
+if (data.length == 0) {
+  console.log("working");
+  watchlist.innerHTML = `
+  <div class="no--saved--container">
+    <h1 class="no--movie--text">You dont have any movies saved yet!</h1>
+    <h1 class="no--movie--text">Go find some</h1>
+    <h1 class="camera">🎥</h1>
+</div>`;
+} else {
+  handleLoading();
+  function remove(id) {
+    for (let i in data) {
+      if (data[i][1] === id) {
+        let index = data.indexOf(data[i]);
+        data.splice(index, 1);
+        window.localStorage.setItem("movieData", JSON.stringify(data));
+      }
+    }
+    console.log(data === []);
+  }
+
+  function handleLoading() {
+    watchlist.innerHTML = "";
+    for (let i in data) {
+      fetch(
+        `https://www.omdbapi.com/?i=${data[i][1]}&plot=short&apikey=95ff048`
+      )
+        .then((jsoned) => jsoned.json())
+        .then((info) => {
+          watchlist.innerHTML += `
+                      <div class="movie--card save--movie" >
+                          <div class="movie--body save--movie--body">
+                              <div class="controlImg">
+                                  <img class="movie--poster" src=${info.Poster} />
+                              </div>
+                              <div class="info--container saved--info--container">
+                                  <h1 class="movie--title">${info.Title}</h1>
+                                  <div class="genreRuntime--container">
+                                      <p class="movie--genre">${info.Genre}</p>
+                                      <p class="info--separators">|</p>
+                                      <p class="movie--duration">${info.Runtime}</p>
+                                      <p class="info--separators">|</p>
+                                      <p class="movie--rating">${info.imdbRating}</p><p class="star">⭐</p>
+                                      <button class="remove--btn" onclick="remove('${info.imdbID}')"><span class="minus"></span></button>
+                                  </div>
+                                  <p class="movie--plot">${info.Plot}</p>
+                                   <span></span>
+                              </div>
+                          </div>
+                     </div>
+                     <span></span> `;
+        });
+
+      //  document.getElementById("remove--btn").addEventListener("click", () => remove(info.Title))})
+    }
+  }
+}
